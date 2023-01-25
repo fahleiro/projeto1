59fec647281b2ad2f41c31a8f3fee5b97b4981da
Gabriel Reinisch Faleiro <40614718+fahleiro@users.noreply.github.com>
2023-01-24 22:12:52 -0300

    Delete conexao.php

--- src/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', '***');
-define('SENHA', '***');
-define('DB', '***');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
